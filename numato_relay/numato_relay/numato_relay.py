f7ff30758192b32bdd2bc7ccd632bd4c0aea7b2a
Rhys Faultless <rfaultless@clearpathrobotics.com>
2023-10-05 13:37:14 -0400

    Added checks for type and value ranges.

--- numato_relay/numato_relay/numato_relay.py
+++ numato_relay/numato_relay/numato_relay.py
@@ -19,26 +19,42 @@ class NumatoRelay(Node):
         
 
     def set_relay(self, request, response):
-        if (request.relay_state):
-            relay_state = 'on' # 'on' or 'off' of whether the relay should be Closed or Open ( on = True = Closed = relay powered )
-            response.relay_response = True
+        response.relay_response_string = ' '
+        response.relay_response_bool = False
+        if (type(request.relay_state) is bool):     
+            if (type(request.relay_channel) is int):
+                if (not (request.relay_channel < 0) ):
+                    if ( not(request.relay_channel > (self.get_number_of_relays()-1) ) ):
+                        if (not (self.get_relay_state(request.relay_channel) == request.relay_state) ): # Check if the requested relay_channel is already set to the requested relay_state
+                            if (request.relay_state):
+                                relay_state = 'on' # 'on' or 'off' of whether the relay should be Closed or Open ( on = True = Closed = relay powered )
+                                response.relay_response_bool = True
+                                response.relay_response_string = 'Relay has been turned on.'
+                            else:
+                                relay_state = 'off'
+                                response.relay_response_bool = False
+                                response.relay_response_string = 'Relay has been turned off.'
+                            self.change_relay_state(request.relay_channel, request.relay_state)
+                            self.serial_lock.acquire()
+                            self.serial_port.write(f"relay {relay_state} {request.relay_channel}\n\r".encode("utf-8"))
+                            self.serial_port.flush()
+                            self.serial_lock.release()
+                        else:
+                            print('Relay is already set to ' + str(request.relay_state) )
+                            response.relay_response_string = 'Relay is already set to ' + str(request.relay_state)
+                    else:
+                        print('The requested relay_channel is larger than what this Relay PCBA supports')
+                        response.relay_response_string = 'The requested relay_channel is larger than what this Relay PCBA supports'
+                else:
+                    print('The requested relay_channel needs to be a positive Integer')
+                    response.relay_response_string = 'The requested relay_channel needs to be a positive Integer'
+            else:
+                print('The requested relay_channel needs to be an Integer')
+                response.relay_response_string = 'The requested relay_channel needs to be an Integer'
         else:
-            relay_state = 'off'
-            response.relay_response = False
-
-        relay_channel = request.relay_channel # Integer starting from 0, aligned with what physical relay to operate
-    
-        # Check if the requested relay_channel is already set to the requested relay_state
-        if self.get_relay_state(relay_channel) == request.relay_state:
-            print('Relay ' + str(relay_channel) + ' is already set to ' + str(relay_state) + ' / ' + str(response.relay_response) )
-        else:
-            self.set_relay_state(relay_channel, relay_state)
-            self.serial_lock.acquire()
-            self.serial_port.write(f"relay {relay_state} {relay_channel}\n\r".encode("utf-8"))
-            self.serial_port.flush()
-            self.serial_lock.release()
-
-        return response   
+            print('The requested relay_state needs to be an Bool')
+            response.relay_response_string = 'The requested relay_state needs to be an Bool'
+        return response
 
 
     def read_relay(self):
