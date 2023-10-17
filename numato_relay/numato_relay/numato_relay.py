cca28374cdd36969a6374e06e473c43dadbd2491
Rhys Faultless <rfaultless@clearpathrobotics.com>
2023-10-17 14:56:50 -0400

    Added GPIO reading, and topic publishing.

--- numato_relay/numato_relay/numato_relay.py
+++ numato_relay/numato_relay/numato_relay.py
@@ -3,7 +3,6 @@ from threading import Lock
 from numato_relay_interfaces.srv import SetRelay
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
 from std_msgs.msg import Bool
 
 
@@ -18,6 +17,7 @@ class NumatoRelay(Node):
         self.serial_port = serial.Serial(self.port, self.baud, timeout=1)
         self.SERIAL_READ_SIZE = 25
         self.serial_lock = Lock()
+
         self.relay_state_array = []
         self.relay_publisher_0 = None
         self.relay_publisher_1 = None
@@ -27,6 +27,16 @@ class NumatoRelay(Node):
         self.relay_publisher_5 = None
         self.relay_publisher_6 = None
         self.relay_publisher_7 = None
+
+        self.gpio_state_array = []
+        self.gpio_publisher_0 = None
+        self.gpio_publisher_1 = None
+        self.gpio_publisher_2 = None
+        self.gpio_publisher_3 = None
+        self.gpio_publisher_4 = None
+        self.gpio_publisher_5 = None
+        self.gpio_publisher_6 = None
+        self.gpio_publisher_7 = None
         
 
     def set_relay(self, request, response):
@@ -116,6 +126,60 @@ class NumatoRelay(Node):
             self.relay_publisher_7 = self.create_publisher(Bool, '/numato_relay_state_7', 10)
 
 
+    def read_gpio(self):
+        index_count = 0
+        while True:
+            self.serial_lock.acquire()
+            self.serial_port.write(f"gpio read {index_count} \n\r".encode("utf-8"))
+            response = str(self.serial_port.read(self.SERIAL_READ_SIZE))
+            self.serial_port.flush()
+            self.serial_lock.release()
+            response = response.lstrip(response[0:13])
+            if '1' in response:
+                self.set_gpio_state(index_count, True)
+            elif '0' in response:
+                self.set_gpio_state(index_count, False)
+            else:
+                break
+            index_count += 1
+
+
+    def change_gpio_state(self, index, content):
+        self.gpio_state_array[index] = content
+
+
+    def set_gpio_state(self, index, content):
+        self.gpio_state_array.insert(index, content)
+
+
+    def get_gpio_state(self, index):
+        return self.gpio_state_array[index]
+
+
+    def get_number_of_gpio(self):
+        return len(self.gpio_state_array)
+
+
+    def update_gpio_publishers(self):
+        number_of_gpio = self.get_number_of_gpio()
+        if ( number_of_gpio > 0 ):
+            self.gpio_publisher_0 = self.create_publisher(Bool, '/numato_gpio_state_0', 10)
+        if ( number_of_gpio > 1 ):
+            self.gpio_publisher_1 = self.create_publisher(Bool, '/numato_gpio_state_1', 10)
+        if ( number_of_gpio > 2 ):
+            self.gpio_publisher_2 = self.create_publisher(Bool, '/numato_gpio_state_2', 10)
+        if ( number_of_gpio > 3 ):
+            self.gpio_publisher_3 = self.create_publisher(Bool, '/numato_gpio_state_3', 10)
+        if ( number_of_gpio > 4 ):
+            self.gpio_publisher_4 = self.create_publisher(Bool, '/numato_gpio_state_4', 10)
+        if ( number_of_gpio > 5 ):
+            self.gpio_publisher_5 = self.create_publisher(Bool, '/numato_gpio_state_5', 10)
+        if ( number_of_gpio > 6 ):
+            self.gpio_publisher_6 = self.create_publisher(Bool, '/numato_gpio_state_6', 10)
+        if ( number_of_gpio > 7 ):
+            self.gpio_publisher_7 = self.create_publisher(Bool, '/numato_gpio_state_7', 10)
+
+
     def timer_callback(self):
         if ( not(self.relay_publisher_0 == None) ):
             msg_0 = Bool()
@@ -165,12 +229,63 @@ class NumatoRelay(Node):
             self.relay_publisher_7.publish(msg_7)
             self.get_logger().info('Publishing relay 7: "%s"' % msg_7.data)
 
+        self.read_gpio()
+
+        if ( not(self.gpio_publisher_0 == None) ):
+            gpio_msg_0 = Bool()
+            gpio_msg_0.data = self.get_gpio_state(0)
+            self.gpio_publisher_0.publish(gpio_msg_0)
+            self.get_logger().info('Publishing GPIO 0: "%s"' % gpio_msg_0.data)
+
+        if ( not(self.gpio_publisher_1 == None) ):
+            gpio_msg_1 = Bool()
+            gpio_msg_1.data = self.get_gpio_state(1)
+            self.gpio_publisher_1.publish(gpio_msg_1)
+            self.get_logger().info('Publishing GPIO 1: "%s"' % gpio_msg_1.data)
+
+        if ( not(self.gpio_publisher_2 == None) ):
+            gpio_msg_2 = Bool()
+            gpio_msg_2.data = self.get_gpio_state(2)
+            self.gpio_publisher_2.publish(gpio_msg_2)
+            self.get_logger().info('Publishing GPIO 2: "%s"' % gpio_msg_2.data)
+
+        if ( not(self.gpio_publisher_3 == None) ):
+            gpio_msg_3 = Bool()
+            gpio_msg_3.data = self.get_gpio_state(3)
+            self.gpio_publisher_3.publish(gpio_msg_3)
+            self.get_logger().info('Publishing GPIO 3: "%s"' % gpio_msg_3.data)
+
+        if ( not(self.gpio_publisher_4 == None) ):
+            gpio_msg_4 = Bool()
+            gpio_msg_4.data = self.get_gpio_state(4)
+            self.gpio_publisher_4.publish(gpio_msg_4)
+            self.get_logger().info('Publishing GPIO 4: "%s"' % gpio_msg_4.data)
+
+        if ( not(self.gpio_publisher_5 == None) ):
+            gpio_msg_5 = Bool()
+            gpio_msg_5.data = self.get_gpio_state(5)
+            self.gpio_publisher_5.publish(gpio_msg_5)
+            self.get_logger().info('Publishing GPIO 5: "%s"' % gpio_msg_5.data)
+
+        if ( not(self.gpio_publisher_6 == None) ):
+            gpio_msg_6 = Bool()
+            gpio_msg_6.data = self.get_gpio_state(6)
+            self.gpio_publisher_6.publish(gpio_msg_6)
+            self.get_logger().info('Publishing GPIO 6: "%s"' % gpio_msg_6.data)
+
+        if ( not(self.gpio_publisher_7 == None) ):
+            gpio_msg_7 = Bool()
+            gpio_msg_7.data = self.get_gpio_state(7)
+            self.gpio_publisher_7.publish(gpio_msg_7)
+            self.get_logger().info('Publishing GPIO 7: "%s"' % gpio_msg_7.data)
 
 def main():
     rclpy.init()
     numato_relay = NumatoRelay()
     numato_relay.read_relay()
     numato_relay.update_relay_publishers()
+    numato_relay.read_gpio()
+    numato_relay.update_gpio_publishers()
     rclpy.spin(numato_relay)
     numato_relay.destroy_node()
     rclpy.shutdown()
